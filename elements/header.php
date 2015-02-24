1ad3d144f8636e40b7e88546237b7e9e05186409
Scott Llewellyn <scottify@Scotts-MacBook-Air.local>
2015-02-24 18:55:34 +0000

    LESS file setup

--- elements/header.php
+++ elements/header.php
@@ -1,8 +1,12 @@
 <header id="header" role="header">
 	<div class="row">
-		<div class="columns col-8 centered">
+		<div class="columns col-12">
 			<h1 class="title">Scott Llewellyn</h1>
 			<h2 class="strapline">Front End Developer, Visual Designer and all round Geek.</h2>
+		</div>
+	</div>
+	<div class="row">
+		<div class="columns col-8 centered">
 			<p class="paragraph">Currently working as part of a remote team for THINK Digital, specialising in designing and building interesting and worthwhile projects for nonprofit organisations around the world.</p>
 		</div>
 	</div>
