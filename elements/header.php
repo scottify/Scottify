048feec5470a31fea2f3542ef272d4baba25f32e
Scott Llewellyn <scottify@Scotts-MacBook-Air.local>
2015-02-28 21:28:47 +0000

    Header styling added

--- elements/header.php
+++ elements/header.php
@@ -1,13 +1,12 @@
 <header id="header" role="header">
 	<div class="row">
 		<div class="columns col-12">
+			<div class="logo">
+				<img src="images/avatar.jpg" alt="Scott Llewellyn" title="Scott Llewellyn" />
+			</div>
 			<h1 class="title">Scott Llewellyn</h1>
-			<h2 class="strapline">Front End Developer, Visual Designer and all round Geek.</h2>
-		</div>
-	</div>
-	<div class="row">
-		<div class="columns col-8 centered">
-			<p class="paragraph">Currently working as part of a remote team for THINK Digital, specialising in designing and building interesting and worthwhile projects for nonprofit organisations around the world.</p>
+			<h2 class="strapline">Front End Developer, Visual Designer and all round geek.</h2>
+			<p class="paragraph">Currently working as part of a remote team for <a href="http://thinkds.org/" title="THINK Digital" target="_blank">THINK Digital</a>, specialising in designing and building interesting and worthwhile projects for nonprofit organisations around the world.</p>
 		</div>
 	</div>
 </header>
