a80c3eb3f056c39eb202d39cd63865a3eba21f35
Scott Llewellyn <scottify@Scotts-MacBook-Air.local>
2015-03-01 20:14:09 +0000

    Featured Work styling added

--- js/global.js
+++ js/global.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
 	$('.slider').bxSlider({
   		speed: 700,
-  		infiniteLoop: true
+  		infiniteLoop: true,
+  		nextText: '&#xf054;',
+  		prevText: '&#xf053;'
 	});
+	$('.bx-pager').wrapInner('<div class="bx-pager-inner"></div>');
 }); 
--- sections/featured_work.php
+++ sections/featured_work.php
@@ -1,22 +1,17 @@
 <section id="work" class="section">
 	<div class="row">
 		<div class="columns col-12">
-			<h2>Featured Work</h2>
+			<h2 class="subtitle">Featured Work</h2>
 		</div>
 	</div>
 	<div class="slider">
   		<!-- so:slide -->
 		<article class="slide">
 			<div class="row">
-				<div class="columns col-12">
+				<div class="columns col-6">
 					<h3 class="title">Amnesty International</h3>
 					<h4 class="role">Visual Design and Front End Development</h4>
-					<div class="preview">
-						<img src="/" alt="Amnesty International"/>
-					</div>
 					<p class="description">A full responsive, multilingual petition and donation platform for Amnesty International on behalf of Raif Badawi, a Saudi blogger, sentenced to 10 years imprisonment and 1,000 lashes for peacefully exercising his right to freedom of expression.</p>
-					<p class="note"></p>
-					<a class="link" href="https://rapidresponse.amnesty.org/gb" target="_blank">View Project</a>
 					<ul class="skills">
 						<li>Photoshop</li>
 						<li>HTML</li>
@@ -25,6 +20,13 @@
 						<li>PHP</li>
 						<li>MySQL</li>
 					</ul>
+					<p class="note">This campaign has recieved over 1 Million petition signitures.</p>
+					<a class="button" href="https://rapidresponse.amnesty.org/gb" target="_blank">View Project</a>
+				</div>
+				<div class="columns col-6">
+					<div class="preview">
+						<img src="images/projects/project_amnesty.jpg" alt="Amnesty International" title="Amnesty International"/>
+					</div>
 				</div>
 			</div>
 		</article>
@@ -32,15 +34,10 @@
 		<!-- so:slide -->
 		<article class="slide">
 			<div class="row">
-				<div class="columns col-12">
+				<div class="columns col-6">
 					<h3 class="title">UNHCR</h3>
 					<h4 class="role">Visual Design and Front End Development</h4>
-					<div class="preview">
-						<img src="/" alt="UNHCR"/>
-					</div>
 					<p class="description">A fully responsive, multilingual donation platform for UNHCR, the UN’s refugee agency, for campaigns in countries such as Syria and Iraq to help safeguard the rights and well-being of refugees and those seeking asylum.</p>
-					<p class="note"></p>
-					<a class="link" href="https://donateto.unhcr.org/gb/general" target="_blank">View Project</a>
 					<ul class="skills">
 						<li>Photoshop</li>
 						<li>HTML</li>
@@ -49,6 +46,12 @@
 						<li>PHP</li>
 						<li>MySQL</li>
 					</ul>
+					<a class="button" href="https://donateto.unhcr.org/gb/general" target="_blank">View Project</a>
+				</div>
+				<div class="columns col-6">
+					<div class="preview">
+						<img src="images/projects/project_unhcr.jpg" alt="UNHCR" title="UNHCR"/>
+					</div>
 				</div>
 			</div>
 		</article>
@@ -56,15 +59,10 @@
 		<!-- so:slide -->
 		<article class="slide">
 			<div class="row">
-				<div class="columns col-12">
+				<div class="columns col-6">
 					<h3 class="title">Karuna</h3>
 					<h4 class="role">Visual Design and Front End Development</h4>
-					<div class="preview">
-						<img src="/" alt="Karuna"/>
-					</div>
 					<p class="description">A full responsive donation platform for Karuna, a charity working with local community groups to help South Asia’s poorest people escape the hell of poverty and discrimination, with access to their legal rights and to participate fully in society.</p>
-					<p class="note">This project was started and finished in a week.</p>
-					<a class="link" href="http://www.karuna.org/" target="_blank">View Project</a>
 					<ul class="skills">
 						<li>Photoshop</li>
 						<li>HTML</li>
@@ -73,6 +71,13 @@
 						<li>PHP</li>
 						<li>MySQL</li>
 					</ul>
+					<p class="note">This project was started and finished in a week.</p>
+					<a class="button" href="http://www.karuna.org/" target="_blank">View Project</a>
+				</div>
+				<div class="columns col-6">
+					<div class="preview">
+						<img src="images/projects/project_karuna.jpg" alt="Karuna" title="Karuna"/>
+					</div>
 				</div>
 			</div>
 		</article>
@@ -80,21 +85,23 @@
 		<!-- so:slide -->
 		<article class="slide">
 			<div class="row">
-				<div class="columns col-12">
+				<div class="columns col-6">
 					<h3 class="title">Toronto General Hospital</h3>
 					<h4 class="role">Visual Design and Front End Development</h4>
-					<div class="preview">
-						<img src="/" alt="Toronto General Hospital"/>
-					</div>
 					<p class="description">A responsive platform for Give2Live, a campaign help raise money for the Transplant Patient &amp; Family Support Fund at Toronto General Hospital, championed by campaigner and former patient Hélène Campbell.</p>
-					<p class="note">This campaign won the 'Best Social Media Video Campaign' award from the <a href="https://www.facebook.com/Give2LiveCampaign" title="'Best Social Media Video Campaign' award from the Association of Healthcare Philanthropy in 2013" target="_blank">Association of Healthcare Philanthropy in 2013</a>.</p>
-					<a class="link" href="http://www.tgwhfonline.ca/give2live/index.php " target="_blank">View Project</a>
 					<ul class="skills">
 						<li>Photoshop</li>
 						<li>HTML</li>
 						<li>CSS</li>
 						<li>JQuery</li>
 					</ul>
+					<p class="note">This campaign won the 'Best Social Media Video Campaign' award from the <a href="https://www.facebook.com/Give2LiveCampaign" title="'Best Social Media Video Campaign' award from the Association of Healthcare Philanthropy in 2013" target="_blank">Association of Healthcare Philanthropy in 2013</a>.</p>
+					<a class="button" href="http://www.tgwhfonline.ca/give2live/index.php " target="_blank">View Project</a>
+				</div>
+				<div class="columns col-6">
+					<div class="preview">
+						<img src="images/projects/project_give2live.jpg" alt="Give2Live" title="Give2Live"/>
+					</div>
 				</div>
 			</div>
 		</article>
@@ -102,21 +109,23 @@
 		<!-- so:slide -->
 		<article class="slide">
 			<div class="row">
-				<div class="columns col-12">
+				<div class="columns col-6">
 					<h3 class="title">Royal Agricultural University</h3>
 					<h4 class="role">Front End Development</h4>
-					<div class="preview">
-						<img src="/" alt="Royal Agricultural University"/>
-					</div>
 					<p class="description">A vast online platform for the Royal Agricultural University (formerly Royal Agricultural College), the oldest agricultural college in the English speaking world, offering courses at foundation, undergraduate and postgraduate levels to over 1,000 students from over 40 different countries.</p>
-					<p class="note">'Best Corporate Website' at the <a href="http://www.rau.ac.uk/news/college-news/beef-get-mobile-for-the-rac" title="'Best Corporate Website' at the CorpComms Awards 2010" target="_blank">CorpComms Awards 2010</a>.</p>
-					<a class="link" href="http://www.rau.ac.uk/" target="_blank">View Project</a>
 					<ul class="skills">
 						<li>HTML</li>
 						<li>CSS</li>
 						<li>JQuery</li>
 						<li>Ruby On Rails</li>
 					</ul>
+					<p class="note">'Best Corporate Website' at the <a href="http://www.rau.ac.uk/news/college-news/beef-get-mobile-for-the-rac" title="'Best Corporate Website' at the CorpComms Awards 2010" target="_blank">CorpComms Awards 2010</a>.</p>
+					<a class="button" href="http://www.rau.ac.uk/" target="_blank">View Project</a>
+				</div>
+				<div class="columns col-6">
+					<div class="preview">
+						<img src="images/projects/project_rau.jpg" alt="Royal Agricultural University" title="Royal Agricultural University"/>
+					</div>
 				</div>
 			</div>
 		</article>
@@ -124,21 +133,23 @@
 		<!-- so:slide -->
 		<article class="slide">
 			<div class="row">
-				<div class="columns col-12">
+				<div class="columns col-6">
 					<h3 class="title">Gap Medics</h3>
 					<h4 class="role">Front End Development</h4>
-					<div class="preview">
-						<img src="/" alt="Gap Medics"/>
-					</div>
 					<p class="description">A visually stunning website for Gap Medics, a specialist company dedicated to providing year-round hospital work experience placements abroad to young students help them gain valuable experience to apply for medical school.</p>
-					<p class="note">This site was featured as one of the ‘Best of CSS Design 2009’ sites on the popular design blog, <a href="http://webdesignerwall.com/trends/best-of-css-design-2009" title="‘Best of CSS Design 2009’ on Web Design Wall" target="_blank">Web Design Wall</a>.</p>
-					<a class="link" href="http://www.gapmedics.co.uk/" target="_blank">View Project</a>
 					<ul class="skills">
 						<li>HTML</li>
 						<li>CSS</li>
 						<li>JQuery</li>
 						<li>Ruby On Rails</li>
 					</ul>
+					<p class="note">This site was featured as one of the ‘Best of CSS Design 2009’ sites on the popular design blog, <a href="http://webdesignerwall.com/trends/best-of-css-design-2009" title="‘Best of CSS Design 2009’ on Web Design Wall" target="_blank">Web Design Wall</a>.</p>
+					<a class="button" href="http://www.gapmedics.co.uk/" target="_blank">View Project</a>
+				</div>
+				<div class="columns col-6">
+					<div class="preview">
+						<img src="images/projects/project_gap_medics.jpg" alt="Gap Medics" title="Gap Medics"/>
+					</div>
 				</div>
 			</div>
 		</article>
